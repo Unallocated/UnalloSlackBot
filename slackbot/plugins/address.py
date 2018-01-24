84d0188862939274bb8b77b66b7d1aaf1b8ead92
root <root@localhost.localdomain>
2018-01-24 01:36:17 -0500

    Added address and phone plugins.

---
+++ slackbot/plugins/address.py
@@ -0,0 +1,19 @@
+#coding: UTF-8
+import re
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+
+
+ADDRESS = "512 Shaw Court #105, Severn MD, 21144"
+
+
+@respond_to('address$', re.IGNORECASE)
+def hello_reply(message):
+    message.reply(ADDRESS)
+
+
+@listen_to('address$', re.IGNORECASE)
+def hello_send(message):
+    message.send(ADDRESS)
+
+
